417a5c824636d746f7c0dec5a62f4310cea90d22
SutharShantanu <shantanusut2000@gmail.com>
2023-02-03 20:55:01 +0530

    nav update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <h1>Hello Ji</h1>
+      <header>
+        <nav>
+          <div className="nav_one">
+            <img src="https://assets.website-files.com/6319f69c5ef0f35ae3db6df0/6319f90a93c7120caa5653b3_Logo%20White.svg" alt="" />
+            <h2>Shantanu.Dev</h2>
+          </div>
+          <div className="nav_two">
+            <a href="">About</a>
+            <a href="">Skill</a>
+            <a href="">Project</a>
+            <a href="">Contact</a>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
