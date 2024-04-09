b6f250e526a7bf37d73f3fe817ee59169b54629f
SutharShantanu <shantanusut2000@gmail.com>
2024-04-10 02:12:54 +0530

    fix soome miner issue

--- src/app/projects.jsx
+++ src/app/projects.jsx
@@ -41,7 +41,7 @@ const Projects = () => {
     return (
         <section
             id="projects"
-            className="border min-h-[87vh]  border-gray-200 w-full 2xl:w-4/5 xl:w-5/6 m-auto my-4 sm:p-8 rounded-lg shadow-sm">
+            className="border  border-gray-200 w-full 2xl:w-4/5 xl:w-5/6 m-auto my-4 sm:p-8 rounded-lg shadow-sm">
             <div className="group inline-block text-left text-5xl w-fit hover:font-bold hover transition-all">
                 Projects
                 <MoveRight
