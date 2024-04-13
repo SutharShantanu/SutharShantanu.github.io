85ccb9b81ba5822ebdc9163e1e5332ac746e0814
SutharShantanu <shantanusut2000@gmail.com>
2024-04-14 03:47:17 +0530

    Dark theme & Navbar adjustment

--- src/Components/Theme.jsx
+++ src/Components/Theme.jsx
@@ -1,40 +1,36 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Moon, SunDim } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
 
 const ThemeSwitcher = () => {
-    const { systemTheme, theme, setTheme } = useTheme();
+    const { setTheme, resolvedTheme } = useTheme();
 
-    const renderThemeChanger = () => {
-        const currentTheme = theme === "system" ? systemTheme : theme;
+    if (resolvedTheme === "dark") {
+        return (
+            <Button
+                variant="outline"
+                size="icon"
+                onClick={() => setTheme("light")}
+                className="rounded-full shadow-sm hover:shadow-none border hover:border-gray-200 dark:hover:border-gray-600 dark:border-gray-700 dark:hover:bg-gray-700 dark:bg-gray-800">
+                <SunDim className=" text-yellow-500 animate-in transition-all duration-75" />
+            </Button>
+        );
+    }
 
-        if (currentTheme === "dark") {
-            return (
-                <Button
-                    variant="outline"
-                    size="icon"
-                    onClick={() => setTheme("light")}
-                    className="rounded-full shadow-sm hover:shadow-none border hover:border-gray-200 dark:border-gray-700">
-                    <SunDim className="w-6 h-6 text-yellow-500 animate-in transition-all duration-75" />
-                </Button>
-            );
-        } else {
-            return (
-                <Button
-                    variant="outline"
-                    size="icon"
-                    onClick={() => setTheme("dark")}
-                    className="rounded-full shadow-sm hover:shadow-none border hover:border-gray-200 dark:border-gray-700">
-                    <Moon className="w-6 h-6 animate-in transition-all duration-75" />
-                </Button>
-            );
-        }
-    };
-
-    return <>{renderThemeChanger()}</>;
+    if (resolvedTheme === "light") {
+        return (
+            <Button
+                variant="outline"
+                size="icon"
+                onClick={() => setTheme("dark")}
+                className="rounded-full shadow-sm hover:shadow-none border hover:border-gray-200 bg-white hover:bg-white">
+                <Moon className=" animate-in transition-all duration-75" />
+            </Button>
+        );
+    }
 };
 
 export default ThemeSwitcher;
