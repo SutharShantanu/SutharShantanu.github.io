2d528253c3fead21fef0bdb192328dda2a3c1b8e
SutharShantanu <shantanusut2000@gmail.com>
2024-04-15 00:04:35 +0530

    about done for mobile

--- src/Components/Splines.jsx
+++ src/Components/Splines.jsx
@@ -4,8 +4,10 @@ import Spline from "@splinetool/react-spline";
 import React from "react";
 
 const Splines = () => {
+    // https://prod.spline.design/urnifnN33kkLXsGY/scene.splinecode
+    // https://prod.spline.design/rCszF0ipJKryfpaK/scene.splinecode
     return (
-        <Spline scene="https://prod.spline.design/urnifnN33kkLXsGY/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/rCszF0ipJKryfpaK/scene.splinecode" />
     );
 };
 
