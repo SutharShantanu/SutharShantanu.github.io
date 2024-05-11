d954ae1e9153a1ee7c6d7de156fe6771647e1e17
SutharShantanu <shantanusut2000@gmail.com>
2024-05-11 16:43:48 +0530

    git commit try

--- src/Components/MobileCarousel.jsx
+++ src/Components/MobileCarousel.jsx
@@ -25,7 +25,7 @@ const MobileCarousel = ({ slides, options }) => {
                                 className="flex-shrink-0 w-full mx-4"
                                 key={index}>
                                 <Image
-                                    className="w-[90%] mx-auto h-[100%] object-cover object-top rounded-md border border-neutral-700"
+                                    className="w-[90%] mx-auto h-[100%] object-cover object-top rounded-md border border-neutral-200 dark:border-neutral-800"
                                     src={ele.image}
                                     alt=""
                                 />
