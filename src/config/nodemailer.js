875365406ec93c75001978b21fafd9024aa7ab27
Shantanu <shantanusut2000@gmail.com>
2024-06-22 23:51:07 +0530

    changes pushed for email work

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,18 +1,17 @@
 import nodemailer from "nodemailer";
 
-const email = process.env.GMAIL_USER
-const pass = process.env.GMAIL_PASS
-
+const email = process.env.NEXT_PUBLIC_GMAIL_USER;
+const pass = process.env.NEXT_PUBLIC_GMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
     service: "gmail.com",
     auth: {
         user: email,
         pass,
-    }
+    },
 });
 
 export const mailOptios = {
     from: email,
     to: email,
-}
+};
