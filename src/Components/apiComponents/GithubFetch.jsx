49ff91250ff777a3bae3728f6defe7b210e98bc1
Shantanu <shantanusut2000@gmail.com>
2024-06-23 16:50:32 +0530

    Github API Data

--- src/Components/apiComponents/GithubFetch.jsx
+++ src/Components/apiComponents/GithubFetch.jsx
@@ -5,39 +5,28 @@ const GithubFetch = () => {
     const [repoData, setRepoData] = useState(null);
     const [specificRepoData, setSpecificRepoData] = useState(null);
     const [error, setError] = useState(null);
-    const [retryIntervalId, setRetryIntervalId] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setError(null);
-
-            try {
-                const response = await fetch("/api/github");
-                const data = await response.json();
-                if (response.status === 200) {
-                    setUserData(data.userData);
-                    setRepoData(data.repoData);
-                    setSpecificRepoData(data.specificRepoData);
-                    clearInterval(retryIntervalId); // Data fetched successfully, stop retrying
-                } else {
-                    throw new Error(data.error || "An error occurred.");
-                }
-            } catch (error) {
-                console.log(`Error: ${error.message}`);
-                setError(error.message);
+    const fetchData = async () => {
+        console.log("GithubFetch called !");
+        setError(null);
+        try {
+            const response = await fetch("/api/github");
+            const data = await response.json();
+            if (response.status === 200) {
+                setUserData(data.userData);
+                setRepoData(data.repoData);
+                setSpecificRepoData(data.specificRepoData);
+            } else {
+                setError(data.error);
+                throw new Error(data.error || "An error occurred.");
             }
-        };
+        } catch (error) {
+            console.log(`Error: ${error.message}`);
+            setError(error.message);
+        }
+    };
 
-        const intervalId = setInterval(() => {
-            fetchData();
-        }, 3000);
-
-        setRetryIntervalId(intervalId);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    fetchData();
 
     return { userData, repoData, specificRepoData, error };
 };
