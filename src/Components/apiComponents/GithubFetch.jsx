28006e810f4894e80520c2bb795ea8777e105dca
Shantanu <shantanusut2000@gmail.com>
2024-06-23 18:39:20 +0530

    API changes in handling

--- src/Components/apiComponents/GithubFetch.jsx
+++ src/Components/apiComponents/GithubFetch.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const GithubFetch = () => {
     const [userData, setUserData] = useState(null);
     const [repoData, setRepoData] = useState(null);
     const [specificRepoData, setSpecificRepoData] = useState(null);
     const [error, setError] = useState(null);
+    const retryIntervalId = useRef(null);
 
     const fetchData = async () => {
         console.log("GithubFetch called !");
@@ -16,6 +17,7 @@ const GithubFetch = () => {
                 setUserData(data.userData);
                 setRepoData(data.repoData);
                 setSpecificRepoData(data.specificRepoData);
+                clearInterval(retryIntervalId.current); // Data fetched successfully, stop retrying
             } else {
                 setError(data.error);
                 throw new Error(data.error || "An error occurred.");
@@ -26,7 +28,21 @@ const GithubFetch = () => {
         }
     };
 
-    fetchData();
+    useEffect(() => {
+        // Retry fetch every 3 seconds
+        retryIntervalId.current = setInterval(() => {
+            if (!userData || !repoData || (repoData && specificRepoData === null)) {
+                fetchData();
+            } else {
+                clearInterval(retryIntervalId.current); // Stop retrying if data is fetched
+            }
+        }, 3000);
+
+        // Cleanup function to clear the interval when the component unmounts
+        return () => {
+            clearInterval(retryIntervalId.current);
+        };
+    }, [userData, repoData, specificRepoData]);
 
     return { userData, repoData, specificRepoData, error };
 };
